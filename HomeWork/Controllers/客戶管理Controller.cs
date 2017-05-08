0133f37791f709e2d7eff99adee603ed3295c0b1
啦啦 呼 <pigkghon@hotmail.com>
2017-05-08 17:53:38 +0800

    加入資料庫

---
+++ HomeWork/Controllers/客戶管理Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeWork.Controllers
+{
+    public class 客戶管理Controller : Controller
+    {
+        // GET: 客戶管理
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
